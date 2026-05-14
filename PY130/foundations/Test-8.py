b9660e8d6ff764d58b24defc25c42e275c71d06f
John Scott <john.scott.seaton@gmail.com>
2026-05-13 20:39:40 -0500

    Add testing exercises

--- PY130/foundations/Test-8.py
+++ PY130/foundations/Test-8.py
@@ -10,3 +10,40 @@ class TestMyList(unittest.TestCase):
         self.assertIs(lst, lst.process())
 
 unittest.main()
+
+
+
+# cat.py
+# --------------------------
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def miaow(self):
+        return f"{self.name} is miaowing."
+
+
+# test_cat.py
+# --------------------------
+import unittest
+from cat import Cat
+
+class CatTests(unittest.TestCase):
+
+    def setUp(self):
+        self.kitty = Cat('Kitty')
+
+    def test_is_cat(self):
+        pass
+
+    def test_name(self):
+        pass
+
+    def test_miaow(self):
+        pass
+
+    def test_raises_error(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
